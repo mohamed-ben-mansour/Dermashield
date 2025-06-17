e23b23d279e3b92dff3b31397bad32b3b371ca6b
mohamed-ben-mansour <mohamedbenmansour70@gmail.com>
2025-06-17 17:39:03 +0100

    adding the testing database

--- gl_version1/settings.py
+++ gl_version1/settings.py
@@ -118,9 +118,12 @@ DATABASES = {
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
         'PORT': os.environ.get('POSTGRES_PORT', '5432'),
-
+        'TEST': {
+            'NAME': os.environ.get('POSTGRES_TEST_DB', os.environ.get('POSTGRES_DB')),
+        }
     }
-}
+    }
+
 
 
 
