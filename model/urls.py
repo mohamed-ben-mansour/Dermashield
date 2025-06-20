b0802ccd1489fa28b628efa6b9746221829a9235
mohamed-ben-mansour <mohamedbenmansour70@gmail.com>
2025-06-20 14:36:18 +0100

    url problem removed

--- model/urls.py
+++ model/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('predict/', views.predict_image, name='predict_image'),
+    # path('predict/', views.predict_image, name='predict_image'),
     path('detect/', views.detect_cancer_risk, name='predict_cancer_risk'),
 
 ]
